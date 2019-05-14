9e566c200397d1383dffb6feefe3d504e7dafe53
Otto Schuldt <ottoschuldt@gmail.com>
2019-05-14 22:27:48 +0200

    adds TestGetUser_Handler

--- pkg/http/rest/handler_test.go
+++ pkg/http/rest/handler_test.go
@@ -99,3 +99,60 @@ func TestAddUser_Handler(t *testing.T) {
 		t.Errorf("expected a user `Fancy Gopher gopher@hotmail.com %v`; got %v %v %v %v", int(groups.GroupA|groups.GroupB), u.FirstName, u.LastName, u.Email, u.GroupConfig)
 	}
 }
+
+func TestGetUser_Handler(t *testing.T) {
+	// request 1
+	r1, err := http.NewRequest(http.MethodGet, "localhost:8080", nil)
+	r1.URL.Path = "/get/1"
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	// request 2
+	r2, err := http.NewRequest(http.MethodGet, "localhost:8080", nil)
+	r2.URL.Path = "/get/2"
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	// request 3
+	r3, err := http.NewRequest(http.MethodGet, "localhost:8080", nil)
+	r3.URL.Path = "/get/2"
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	// setup
+	app := new(App)
+	storage := new(memory.Storage)
+	var lister listing.Service
+	lister = listing.NewService(storage)
+	rec1 := httptest.NewRecorder()
+	rec2 := httptest.NewRecorder()
+	rec3 := httptest.NewRecorder()
+
+	// create user
+	u := adding.User{FirstName: "Fancy", LastName: "Gopher", Email: "gopher@gmail.com", GroupConfig:0}
+	_ = storage.AddUser(u)
+
+	// make requests
+	app.GetUser.Handler(lister).ServeHTTP(rec1, r1)
+	res1 := rec1.Result()
+	app.GetUser.Handler(lister).ServeHTTP(rec2, r2)
+	res2 := rec2.Result()
+	app.GetUser.Handler(lister).ServeHTTP(rec3, r3)
+	res3 := rec3.Result()
+
+	// tests
+	if res1.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res1.StatusCode)
+	}
+
+	if res2.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected status 400; got %v", res2.StatusCode)
+	}
+
+	if res3.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected status 400; got %v", res3.StatusCode)
+	}
+}
