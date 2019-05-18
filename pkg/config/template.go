ead30eb3ebdd7ffa0d7309ded0e5b55d5e22737d
Otto Schuldt <ottoschuldt@gmail.com>
2019-05-18 19:12:28 +0200

    improves template initialization

--- pkg/config/template.go
+++ pkg/config/template.go
@@ -11,18 +11,13 @@ import (
 var TPL *template.Template
 
 func init() {
-	// be aware of duplicated template names, this is done this way for simplicity
-	templateList := []string{
-		"templates/add.gohtml",
-		"templates/list.gohtml",
-		"templates/detail.gohtml",
-	}
-
 	// when testing we need to change the working directory to the root app
 	if flag.Lookup("test.v") != nil {
 		if err := os.Chdir("../../.."); err != nil {
 			log.Fatal(err)
 		}
 	}
-	TPL = template.Must(template.New("").Funcs(groups.Fm).ParseFiles(templateList...))
+
+	// be aware of duplicated template names, this is done this way for simplicity
+	TPL = template.Must(template.New("").Funcs(groups.Fm).ParseGlob("templates/*.gohtml"))
 }
